831c2e59dc06f8ecd16f8676dee2188a26a3dbe6
Renan Bispo <renanjesusbispo@gmail.com>
2024-08-05 10:22:29 -0300

    corrigindo bug

--- src/repositories/BrowserInternet.java
+++ src/repositories/BrowserInternet.java
@@ -5,5 +5,5 @@ import models.Page;
 public interface BrowserInternet {
     void displayPage(Page page);
     void newTab(Page page);
-    void updatePage(Page oldPage, Page newPage);
+    Page updatePage(Page oldPage, Page newPage);
 }
