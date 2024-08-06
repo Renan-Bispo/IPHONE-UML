69b7d136d8e6558370dfb537e5c0a482c10e69c7
Renan Bispo <renanbispouniversity@gmail.com>
2024-08-06 10:19:12 -0300

    utilizando funções de ligação

--- src/Main.java
+++ src/Main.java
@@ -18,17 +18,30 @@ public class Main {
                 new Contact("Maria", 11111111),
                 new Contact("João", 22222222),
                 new Contact("Zé do Pneu", 33333333),
-
+                new Contact("Carlos", 44444444)
         ));
 
 
-        List<VoiceMail> voiceMails = new ArrayList<>(Arrays.asList(new VoiceMail(0, "")));
+        List<VoiceMail> voiceMails = new ArrayList<>(Arrays.asList(
+                new VoiceMail(11111111, "Oi"),
+                new VoiceMail(22222222, "Tenho um recado"),
+                new VoiceMail(33333333, "Me ligue quando puder"),
+                new VoiceMail(44444444, "Vamos nos ver hoje?")
+        ));
 
 
-        List<Page> browserInternet = new ArrayList<>(Arrays.asList(new Page("", "")));
+        List<Page> browserInternet = new ArrayList<>(Arrays.asList(
+                new Page("www.instagram.com", ""),
+                new Page("www.apple.com",""),
+                new Page("www.youtube.com", "")
+        ));
 
         //criando objeto do iphone
         Iphone iphone = new Iphone(musics, contacts, browserInternet, voiceMails);
+        //funções de Telefone do Iphone
+        iphone.call(contacts.get(1));
+        iphone.toMeet();
+        iphone.initVoiceMail();
     }
 
 }
