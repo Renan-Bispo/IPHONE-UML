1884cb935a9b31798c9e7ab9274ea7188507c52f
Renan Bispo <renanjesusbispo@gmail.com>
2024-08-06 09:53:55 -0300

    intanciando funcionalidades do iphone

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
-import models.Iphone;
+import models.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        List<Music> musics = new ArrayList<>(Arrays.asList(new Music("", "")));
 
+
+        List<Contact> contacts = new ArrayList<>(Arrays.asList(new Contact("", 0)));
+
+
+        List<VoiceMail> voiceMails = new ArrayList<>(Arrays.asList(new VoiceMail(0, "")));
+
+
+        List<Page> browserInternet = new ArrayList<>(Arrays.asList(new Page("", "")));
+
+        //criando objeto do iphone
+        Iphone iphone = new Iphone(musics, contacts, browserInternet, voiceMails);
     }
 
 }
